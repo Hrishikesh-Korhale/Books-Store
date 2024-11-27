92366f27f4a427f4eeecc4e4568da56c77f78e4d
Hrishikesh Korhale <hrishikorhale2002@gmail.com>
2024-11-27 23:24:12 +0530

    added book_detail page

--- books/urls.py
+++ books/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.book_list, name='book_list'),
+    path('', views.book_list, name='book_list'),  # Main book listing
+    path('book/<int:id>/', views.book_detail, name='book_detail'),  # Book detail page
 ]
